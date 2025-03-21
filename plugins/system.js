ba4e1970f4e58c0ec6bf5f91d04f44d3527bd8cf
LLWBOT <gamingchamiya344@gmail.com>
2025-03-21 21:21:22 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -10,13 +10,18 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `💠*Uptime:*  ${runtime(process.uptime())}
+let status = `♻️*LLW MD SYSTEM INFO*♻️
+💠*Uptime:*  ${runtime(process.uptime())}
 💠*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
 💠*HostName:* ${os.hostname()}
 💠*Owner:* LLW EDITZ🗣️
 💠*contact llw:* 94705564619
 💠*whatsapp channel:*https://whatsapp.com/channel/0029Vb0s10t6BIEm7YKTHm3R
+
+*LLW MD 2025*💫
 `
+return reply(`${status}`)
+
 }catch(e){
 console.log(e)
 reply(`${e}`)
