505e71519aead6147b2c8090c9439247c5725845
LLWBOT <gamingchamiya344@gmail.com>
2025-03-21 21:16:03 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,14 +1,25 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
-
+const os = require("os")
 cmd({
     pattern: "system",
-    alias: ["system","botinfo"]
-    desc: "Check bot online or no.",
+    alias: ["status","botinfo"]
+    desc: "Check up time , ram usage and more",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+let status = `💠*Uptime:*  ${runtime(process.uptime())}
+💠*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+💠*HostName:* ${os.hostname()}
+💠*Owner:* LLW EDITZ🗣️
+💠*contact llw:* 94705564619
+💠*whatsapp channel:*https://whatsapp.com/channel/0029Vb0s10t6BIEm7YKTHm3R
+`
+}catch(e){
+console.log(e)
+reply(`${e}`)
 
 }
+})
