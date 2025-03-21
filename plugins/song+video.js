92b4ca3aa9b88a47f8868fbfaa723f830ada8f3a
LLWBOT <gamingchamiya344@gmail.com>
2025-03-21 12:45:30 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,1 +1,22 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
 
+
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please provide the name of the music")
+const search = await yts(q)
+
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
