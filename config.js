7735469e152c24f07cb839db5940c38d8a249599
LLWBOT <gamingchamiya344@gmail.com>
2025-03-21 10:38:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "NshBxByA#bCjcmsHy7PWtg7RSijciSBl0pJdvuWuBm5lDu59o3bQ",
+SESSION_ID: process.env.SESSION_ID || "BwxnAKpB#3lw_6mfncW0KRMCD2h9LaYWzwWuXlqyzl5Np3vSbsgs",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/vm5scvzn/Untitled912131311121.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "⚡LLW MD IS ALIVE.THANKS FOR USING LLW BOT🔥.CONTACT OWNER 94705564619",
 };
