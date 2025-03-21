2ded3eba834b1e01ee88e5d6302c06d3730bb13f
LLWBOT <gamingchamiya344@gmail.com>
2025-03-21 19:02:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "1wYlVDTS#nrokG79W9SaRfiYiAqLB1G0o-BkJ083zs9-N3Ad0q0c",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/vm5scvzn/Untitled912131311121.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "⚡LLW MD IS ALIVE.THANKS FOR USING LLW BOT🔥.CONTACT OWNER 94705564619",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
