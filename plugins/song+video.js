dc141fd051a59eeb68262779b0521cbb6128ec54
LLWBOT <gamingchamiya344@gmail.com>
2025-03-21 13:37:28 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -27,7 +27,16 @@ let desc = `
 
  *MADE BY LLW EDITZ*🗿
 `
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc})
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download audio
+    
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio message
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+
 
 
 
