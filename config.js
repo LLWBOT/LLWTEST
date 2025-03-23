c58cd4e1c7ca54c7581510cb725a5f1e132c1cb7
LLWBOT <gamingchamiya344@gmail.com>
2025-03-23 08:53:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "1wYlVDTS#nrokG79W9SaRfiYiAqLB1G0o-BkJ083zs9-N3Ad0q0c",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/vm5scvzn/Untitled912131311121.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "⚡LLW MD IS ALIVE.THANKS FOR USING LLW BOT🔥.CONTACT OWNER 94705564619",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+SESSION_ID: process.env.SESSION_ID || "your session id here",
+MONGODB: process.env.MONGODB || "enter mongodb here"
 };
