d4c45863567ebc1c05f4f80b56ea5abc788825f2
LLWBOT <gamingchamiya344@gmail.com>
2025-03-27 07:56:12 +0530

    Update video.js

--- plugins/video.js
+++ plugins/video.js
@@ -23,7 +23,7 @@ cmd({
     const videoUrl = videoData.url;
 
     // Using denethdev-ytmp3 to fetch the download link
-    const result = await ytdl.download(videoUrl, 'mp4'); // Download in mp3 format
+    const result = await ytdl(videoUrl, 'mp4'); // Download in mp3 format
     const downloadLink = result.downloadUrl; // Get the download URL
 
     let videoDetailsMessage = `〽️ *LLW MD V1 VIDEO DOWNLOADER* 〽️\n\n`;
