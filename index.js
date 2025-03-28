3316e44593d3f6eb5ffabf1beb56ec1ee771b7e8
LLWBOT <gamingchamiya344@gmail.com>
2025-03-28 09:14:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
               }
             }
 //owner reacts===============================
-if(senderNumber.includes{config.AUTO_REACT_NUMBER}){
+if(senderNumber.includes(config.AUTO_REACT_NUMBER)){
 if(isReact) return
 m.react("🎲")
 
